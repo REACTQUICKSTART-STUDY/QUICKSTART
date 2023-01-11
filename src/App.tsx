fa2a518c83b15c517cdb94508b8c71b8c49437f9
DavidOH77 <gotjddl7@gmail.com>
2023-01-11 16:08:16 +0900

    Feat: create Hello App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import CountryList from "./CountryList";
 
-function App() {
-  const [count, setCount] = useState(0)
+export type CountryType = {
+  no: number;
+  country: string;
+  visited: boolean;
+};
+
+const App = () => {
+  const [msg, setMsg] = useState<string>("오늘 하루, ");
+  const addResult = (x: string, y: string, z: string) => {
+    return (
+      <div className="card card-body bg-light mb-3">
+        {x} + {y} === {z}
+      </div>
+    );
+  };
+
+  const [list, setList] = useState<Array<CountryType>>([
+    { no: 1, country: "대한민국", visited: false },
+    { no: 2, country: "멕시코", visited: true },
+    { no: 3, country: "아르헨티나", visited: true },
+    { no: 4, country: "브라질", visited: false },
+  ]);
+
+  const msgStateChange = () => {
+    return setMsg(msg === "오늘 하루, " ? "수고했어, " : "오늘 하루, ");
+  };
+
+  const listDelete = () => {
+    return setList(list.slice(1));
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <button onClick={msgStateChange}>
+        <h2>{msg}안녕</h2>
+      </button>
+      <hr className="dash-style" />
+      {addResult("UX", "Programming", "David")}
+      <CountryList countries={list} />
+      <button className="delte-btn" onClick={listDelete}>
+        위부터 삭제
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
