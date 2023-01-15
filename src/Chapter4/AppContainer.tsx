305cdeef5d806bdac49abc509a02a29a80f113de
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-01-15 23:20:10 +0900

    4.9 TodoList 예제

---
+++ src/Chapter4/AppContainer.tsx
@@ -0,0 +1,52 @@
+import produce from 'immer'
+import React from 'react'
+import { useState } from 'react'
+import App from './Components/App'
+
+export type TodoListItemType = {
+  no: number
+  todo: string
+  done: boolean
+}
+
+const AppContainer = () => {
+  const [todoList, setTodoList] = useState<Array<TodoListItemType>>([
+    { no: 1, todo: "React학습1", done: false },
+    { no: 2, todo: "React학습2", done: false },
+    { no: 3, todo: "React학습3", done: false },
+    { no: 4, todo: "React학습4", done: false }
+  ])
+
+  const addTodo = (todo: string) => {
+    let newTodoList = produce(todoList, (draft) => {
+      draft.push({ no: new Date().getTime(), todo: todo, done: false })
+    })
+    setTodoList(newTodoList)
+  }
+
+  const deleteTodo = (no: number) => {
+    let index = todoList.findIndex((todo) => todo.no === no)
+    let newTodoList = produce(todoList, draft => {
+      draft.splice(index, 1)
+    })
+    setTodoList(newTodoList)
+  }
+
+  const toggleDone = (no: number) => {
+    let index = todoList.findIndex((todo) => todo.no === no)
+    let newTodoList = produce(todoList, draft => {
+      draft[index].done = !draft[index].done
+    })
+  }
+
+  return (
+    <App
+      todoList={todoList}
+      addTodo={addTodo}
+      deleteTodo={deleteTodo}
+      toggleDone={toggleDone}
+    />
+  )
+}
+
+export default AppContainer
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import App from './HyunSoo/App3'
-// import './index.css'
+// import App from './HyunSoo/App3'
+import AppContainer from './Chapter4/AppContainer'
+import "bootstrap/dist/css/bootstrap.css"
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
 )
