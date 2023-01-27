416e5c1c4ee1d4cc347cffa693ee620981a8982a
DavidOH77 <gotjddl7@gmail.com>
2023-01-27 15:46:37 +0900

    Feat: create Class Component Example

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppContatiner from "./AppContatiner";
+import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AppContatiner />
+    <App />
   </React.StrictMode>
 );
