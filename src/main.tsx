8d7cd4ad2ec24b0db8d405d56530dc4a8d6a0090
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-01-29 23:49:22 +0900

    6장 예제 작성중...

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App'
 // import App from './HyunSoo/App3'
-// import AppContainer from './Chapter4/AppContainer'
-import App from "./Chapter51/App";
+import AppContainer from './Chapter4/AppContainer'
+// import App from "./Chapter51/App";
 import "bootstrap/dist/css/bootstrap.css";
 // import './index.css'
 import "./Chapter51/index.css";
