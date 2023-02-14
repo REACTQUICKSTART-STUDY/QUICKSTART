abdecfbe073829477c97d9c3458a093f72d07348
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-14 18:20:10 +0900

    11장 axios를 이용한 HTTP 통신

--- src/main.tsx
+++ src/main.tsx
@@ -10,14 +10,15 @@ import "bootstrap/dist/css/bootstrap.css";
 // import { TodoProvider } from "./Chapter8/TodoContext";
 // import App from "./Chapter8/App";
 // import App from './Chapter9/App'
-import AppContainer from './Chapter10/AppContainer'
+// import AppContainer from './Chapter10/AppContainer'
+import App from './Chapter11/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
   // <TodoProvider>
-    <AppContainer />
-    // <App />
+    // <AppContainer />
+    <App />
   // </TodoProvider>
   // </React.StrictMode>,
 );
