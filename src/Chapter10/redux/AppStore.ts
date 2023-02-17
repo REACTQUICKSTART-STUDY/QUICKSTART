58b63a7f7e42b07e62a1f8824a4c4504ebd2e160
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-17 18:45:11 +0900

    12.5.3 간단한 콘솔 로거 미들웨어

--- src/Chapter10/redux/AppStore.ts
+++ src/Chapter10/redux/AppStore.ts
@@ -10,23 +10,30 @@ export type RootStatesType = {
 
 const RootReducer = combineReducers({ home: TimeReducer, todos: TodoReducer });
 
-const mw1: Middleware = (store) => (next) => (action) => {
-  console.log('### mw1 전')
-  next(action)
-  console.log('### mw1 후')
-}
+// const mw1: Middleware = (store) => (next) => (action) => {
+//   console.log('### mw1 전')
+//   next(action)
+//   console.log('### mw1 후')
+// }
 
-const mw2: Middleware = (store) => (next) => (action) => {
-  console.log('### mw2 전')
+// const mw2: Middleware = (store) => (next) => (action) => {
+//   console.log('### mw2 전')
+//   next(action)
+//   console.log('### mw2 후')
+//   console.log(store.getState())
+// }
+
+const loggerMW: Middleware = (store) => (next) => (action) => {
+  console.log('### action 실행 : ', action)
+  console.log('### action 변경 전 상태 : ', store.getState())
   next(action)
-  console.log('### mw2 후')
-  console.log(store.getState())
+  console.log('### action 변경 후 상태 : ', store.getState())
 }
 
 const AppStore = configureStore({ 
   reducer: RootReducer,
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({ serializableCheck: false}).concat(mw1).concat(mw2)
+    return getDefaultMiddleware({ serializableCheck: false}).concat(loggerMW)
   }
 });
 
