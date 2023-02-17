2b5569e896bb1a7b6bd7f84c0511c3b13bae0815
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-17 18:40:55 +0900

    12.5.2 테스트용 미들웨어 적용하기

--- src/Chapter10/pages/TodoList.tsx
+++ src/Chapter10/pages/TodoList.tsx
@@ -1,35 +1,29 @@
-import { Link } from "react-router-dom"
-import TodoItem from './TodoItem'
-import TodoActionCreator from "../redux/TodoActionCreator"
-import { AnyAction, Dispatch } from "redux"
-import { RootStatesType } from '../redux/AppStore'
-import { connect } from "react-redux"
-import { TodoStatesType, TodoItemType } from '../redux/TodoReducer'
-// import { CallbacksType, StatesType } from "../AppContainer"
+import { Link } from "react-router-dom";
+import TodoItem from "./TodoItem";
+import TodoActionCreator from "../redux/TodoActionCreator";
+import { AnyAction, Dispatch } from "redux";
+import { connect } from "react-redux";
+import { TodoItemType } from "../redux/TodoReducer";
+import { RootStatesType } from "../redux/AppStore";
 
 type PropsType = {
   todoList: Array<TodoItemType>;
   deleteTodo: (id: number) => void;
   toggleDone: (id: number) => void;
-  // states: StatesType;
-  // callbacks: CallbacksType;
-}
+};
 
 const TodoList = ({ todoList, deleteTodo, toggleDone }: PropsType) => {
   let todoItems = todoList.map((item) => {
-    return <TodoItem key={item.id} todoItem={item} 
-    deleteTodo={deleteTodo} toggleDone={toggleDone} />
-  })
+    return <TodoItem key={item.id} todoItem={item} deleteTodo={deleteTodo} toggleDone={toggleDone} />;
+  });
+
   return (
     <>
       <div className="row">
         <div className="col p-3">
-          <Link className="btn btn-primary" to='/todos/add'>
-            할 일 추가
+          <Link className="btn btn-primary" to="/todos/add">
+            할일 추가
           </Link>
-          <button className="btn btn-primary ms-1"
-          // onClick={() => callbacks.fetchTodoList()}
-          >할 일 목록 새로고침</button>
         </div>
       </div>
       <div className="row">
@@ -38,16 +32,16 @@ const TodoList = ({ todoList, deleteTodo, toggleDone }: PropsType) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state: RootStatesType) => ({
-  todoList: state.todos.todoList,
-})
+const mapStateToProps = (states: RootStatesType) => ({
+  todoList: states.todos.todoList,
+});
 
 const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => ({
   deleteTodo: (id: number) => dispatch(TodoActionCreator.deleteTodo({ id })),
   toggleDone: (id: number) => dispatch(TodoActionCreator.toggleDone({ id })),
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/Chapter10/redux/AppStore.ts
+++ src/Chapter10/redux/AppStore.ts
@@ -1,5 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers, Middleware } from "redux";
 import TimeReducer, { HomeStatesType } from "./TimeReducer";
 import TodoReducer, { TodoStatesType } from "./TodoReducer";
 
@@ -8,10 +8,26 @@ export type RootStatesType = {
   todos: TodoStatesType;
 };
 
-const RootReducer = combineReducers({
-  home: TimeReducer,
-  todos: TodoReducer
-})
+const RootReducer = combineReducers({ home: TimeReducer, todos: TodoReducer });
 
-const AppStore = configureStore({ reducer: RootReducer })
-export default AppStore
+const mw1: Middleware = (store) => (next) => (action) => {
+  console.log('### mw1 전')
+  next(action)
+  console.log('### mw1 후')
+}
+
+const mw2: Middleware = (store) => (next) => (action) => {
+  console.log('### mw2 전')
+  next(action)
+  console.log('### mw2 후')
+  console.log(store.getState())
+}
+
+const AppStore = configureStore({ 
+  reducer: RootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({ serializableCheck: false}).concat(mw1).concat(mw2)
+  }
+});
+
+export default AppStore;
