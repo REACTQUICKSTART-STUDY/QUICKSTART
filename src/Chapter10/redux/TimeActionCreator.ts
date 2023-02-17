4e9e6a91ac2dd454824b17451be91e1df1f2f2b0
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-17 19:26:53 +0900

    12.6.4 redux-thunk와 @reduxjs/toolkit 함께 사용하기

--- src/Chapter10/redux/TimeActionCreator.ts
+++ src/Chapter10/redux/TimeActionCreator.ts
@@ -1,36 +1,45 @@
-import { AnyAction } from "redux";
-import { ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
 
-export const TIME_ACTION = {
-  CHANGE_TIME_REQUEST: 'changeTime' as const,
-  CHANGE_TIME_COMPLETED: 'changeTimeCompleted' as const,
-};
+const timeout = 
+  (delayTime: number) => new Promise((resolve) => setTimeout(resolve, delayTime))
+
+// export const TIME_ACTION = {
+//   CHANGE_TIME_REQUEST: 'changeTime' as const,
+//   CHANGE_TIME_COMPLETED: 'changeTimeCompleted' as const,
+// };
 
 // export type TimeActionType = ReturnType<typeof TimeActionCreator.changeTime>;
 
 const TimeActionCreator = {
-  changeTimeRequest: () => {
-    return { type: TIME_ACTION.CHANGE_TIME_REQUEST };
-  },
-  changeTimeCompleted: ({ currentTime }: { currentTime: Date }) => {
-    return {
-      type: TIME_ACTION.CHANGE_TIME_COMPLETED,
-      payload: { currentTime: currentTime },
+  // changeTimeRequest: () => {
+  //   return { type: TIME_ACTION.CHANGE_TIME_REQUEST };
+  // },
+  // changeTimeCompleted: ({ currentTime }: { currentTime: Date }) => {
+  //   return {
+  //     type: TIME_ACTION.CHANGE_TIME_COMPLETED,
+  //     payload: { currentTime: currentTime },
+  //   }
+  // },
+  asyncChangeTime: createAsyncThunk(
+    'changeTime',
+    async (undefined, thunkAPI) => {
+      await timeout(2000)
+      return { currentTime: new Date() }
     }
-  },
-  asyncChangeTime() {
+  ) 
+  // {
     // 의도적 지연 시간 1초
-    return (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
-      dispatch(this.changeTimeRequest())
-      setTimeout(() => {
-        dispatch(this.changeTimeCompleted({ currentTime: new Date() }))
-      }, 1000)
-    }
-  }
+    // return (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
+    //   dispatch(this.changeTimeRequest())
+    //   setTimeout(() => {
+    //     dispatch(this.changeTimeCompleted({ currentTime: new Date() }))
+    //   }, 1000)
+    // }
+  // }
 };
 
-export type TimeActionType = 
-  | ReturnType<typeof TimeActionCreator.changeTimeCompleted>
-  | ReturnType<typeof TimeActionCreator.changeTimeRequest>
+// export type TimeActionType = 
+//   | ReturnType<typeof TimeActionCreator.changeTimeCompleted>
+//   | ReturnType<typeof TimeActionCreator.changeTimeRequest>
 
 export default TimeActionCreator;
--- src/Chapter10/redux/TimeReducer.ts
+++ src/Chapter10/redux/TimeReducer.ts
@@ -1,4 +1,5 @@
-import { TimeActionType, TIME_ACTION } from "./TimeActionCreator";
+import { createReducer } from "@reduxjs/toolkit";
+import TimeActionCreator from "./TimeActionCreator";
 
 const initialState = {
   currentTime: new Date(),
@@ -7,15 +8,26 @@ const initialState = {
 
 export type HomeStatesType = { currentTime: Date, isChanging: boolean };
 
-const TimeReducer = (state: HomeStatesType = initialState, action: TimeActionType) => {
-  switch (action.type) {
-    case TIME_ACTION.CHANGE_TIME_REQUEST:
-      return { ...state, isChanging: true };
-    case TIME_ACTION.CHANGE_TIME_COMPLETED:
-      return { ...state, currentTime: action.payload.currentTime, isChanging: false };
-    default:
-      return state;
-  }
-};
+const TimeReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(TimeActionCreator.asyncChangeTime.pending, (state, action) => {
+      state.isChanging = true
+    })
+    .addCase(TimeActionCreator.asyncChangeTime.fulfilled, (state, action) => {
+      state.currentTime = action.payload.currentTime
+      state.isChanging = false
+    })
+
+}) 
+// {
+  // switch (action.type) {
+  //   case TIME_ACTION.CHANGE_TIME_REQUEST:
+  //     return { ...state, isChanging: true };
+  //   case TIME_ACTION.CHANGE_TIME_COMPLETED:
+  //     return { ...state, currentTime: action.payload.currentTime, isChanging: false };
+  //   default:
+  //     return state;
+  // }
+// };
 
 export default TimeReducer;
